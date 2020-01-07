364b185e79add7a52cf03fe303c7cd01ab25d65e
Scott Chappel <scott.chappel@zapbi.com>
2020-01-07 13:04:38 +1000

    Setup project to demonstrate failure

---
+++ BuildProject/Build.cs
@@ -0,0 +1,26 @@
+﻿using Nuke.Common;
+using Nuke.Common.Execution;
+using Nuke.Common.ProjectModel;
+using Nuke.Common.Tools.DotNet;
+using Nuke.Common.Tools.MSBuild;
+
+namespace BuildProject
+{
+    [CheckBuildProjectConfigurations]
+    [UnsetVisualStudioEnvironmentVariables]
+    [DotNetVerbosityMapping]
+    [MSBuildVerbosityMapping]
+    public class Builder : NukeBuild
+    {
+        public static int Main() => Execute<Builder>(x => x.Compile);
+        
+        [Solution]
+        protected readonly Solution Solution;
+       
+        Target Compile => _ => _
+            .Executes(() =>
+                {
+                    
+                });
+    }
+}
